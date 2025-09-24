10fb74754b0ecc6c670d661113012e17f6ec1c53
Casey <jelsema.casey@gmail.com>
2025-09-24 17:25:16 -0600

    minor tinkering, adjust favicon and get template for drop-down

--- make_beta.R
+++ make_beta.R
@@ -10,6 +10,10 @@ library("tidyverse")
 x <- seq(0,1,length.out=1000)
 y <- dbeta( x, 2, 11 )
 
+
+png( "beta.png", width = 600 , height = 600  )
+
+
 par( mar=c(0,0,0,0) )
 
 plot( 
@@ -17,7 +21,7 @@ plot(
   xaxt='n', yaxt='n',
   xlab="", ylab="",
   col="steelblue3",
-  lwd=2,
+  lwd=6,
   frame=FALSE,
   xlim=c(0,0.75)
 )
@@ -26,11 +30,16 @@ points(
   x = rep( qbeta(0.9,2,11), 2 ),
   y = c(0,dbeta(qbeta(0.9,2,11),2,11)),
   type="l", col="indianred3",
-  lwd=2
+  lwd=6
 )
 
 points(
   x=c(0,0.75),
   y=c(0,0),
-  type="l", col="black",
+  type="l", col="black", lwd=4
 )
+
+# export size:
+
+dev.off()
+
